d45092ae7a240489eabf8eca71558f593124ffda
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2024-11-26 12:03:14 +0100

    removed unecessaruy code and packages

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
 module.exports = {
   mongoDB: {
-    e18connectionString: process.env.MONGO_DB_E18_CONNECTION_STRING ?? null,
-    e18database: process.env.MONGO_DB_E18_DATABASE ?? 'tulliballa',
-    e18StatisticsCollection: process.env.MONGO_DB_E18_STATISTICS_COLLECTION ?? 'jauddaaa',
     statisticsConnectionString: process.env.MONGO_STATISTICS_CONNECTION_STRING ?? 'tullball',
     statisticsDatabase: process.env.MONGO_DB_STATISTICS_DATABASE ?? 'tulliballa'
   },
